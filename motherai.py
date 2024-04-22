af028cbffeb0fd5c8bb4c74567eabfe0d60cc272
zorluhan <zorluhan@gmail.com>
2024-04-22 11:03:54 +0300

    st secret eklendi

--- motherai.py
+++ motherai.py
@@ -7,7 +7,8 @@ current_time=datetime.now()
  
 # Initialize Gemini Pro Chat 
  
-api_key = os.getenv('GOOGLE_API_KEY')
+
+api_key = st.secrets["GOOGLE_API_KEY"]
 genai.configure(api_key=api_key)
 generation_config = {
   "temperature": 0.9,
