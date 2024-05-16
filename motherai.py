8aa1c2eba2b638a9ead8ebbacb2e7b420be0483a
zorluhan <zorluhan@gmail.com>
2024-05-16 12:42:36 +0300

    1.5pro

--- motherai.py
+++ motherai.py
@@ -36,7 +36,7 @@ safety_settings = [
   },
 ]
 
-model = genai.GenerativeModel(model_name="gemini-1.5-flash-latest",
+model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
  
